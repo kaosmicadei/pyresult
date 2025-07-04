d25633e00bebec8bc703b68a6004c6071bbbfc9e
Kaonan Micadei <6026964+kaosmicadei@users.noreply.github.com>
2025-07-04 14:58:55 +0200

    update example

--- examples/pipeline.py
+++ examples/pipeline.py
@@ -7,6 +7,9 @@ from pyresult import Result, Iter, lift
 logger = logging.getLogger(__name__)
 
 
+# An example of benchmark pipeline that processes multiple input files,
+# runs a task on each file, aggregates the results, summarizes them,
+# and saves the final report to an output file.
 def benchmark(
     input_files: list[Path], output_file: Path
 ) -> Result[Exception, None]:
@@ -16,15 +19,15 @@ def benchmark(
         .flatten_result()  # Extract successful results only: Result[E, Iter[T]]
         .and_then(aggregate_results)
         .and_then(summarize)
-        .and_then(lambda r: save(r, output_file))
+        .and_then(lambda report: save(report, output_file))
         .on_err(logger.error)
     )
 
 
-# Opaque functions to represent the steps in the pipeline
-T = TypeVar("T")  # Input type for the algorithm
-U = TypeVar("U")  # Output type for the algorithm
-E = TypeVar("E", bound=BaseException)  # Error type for the algorithm
+# Opaque functions to represent the steps in the pipeline.
+T = TypeVar("T")  # Input type
+U = TypeVar("U")  # Output type
+E = TypeVar("E", bound=BaseException)  # Error type
 
 
 def run_task(file: Path) -> Result[E, T]:
@@ -39,26 +42,12 @@ def run_task(file: Path) -> Result[E, T]:
 def load(file: Path) -> T: ...
 
 
-@lift.result
-def aggregate_results(data: Iter[T]) -> U: ...
-
-
-@lift.result
-def summarize(data: T) -> U: ...
-
-
-@lift.result
-def save(data: T, file: Path) -> None: ...
-
-
-# === Example!
 def execute_algorithm(data: T) -> Result[E, U]:
     return (
         embed(data)
         .and_then(optimize_loop)
         .and_then(decode)
     )
-
 
 @lift.result
 def embed(data: T) -> U: ...
@@ -70,3 +59,15 @@ def optimize_loop(data: T) -> U: ...
 
 @lift.result
 def decode(data: T) -> U: ...
+
+
+@lift.result
+def aggregate_results(data: Iter[T]) -> U: ...
+
+
+@lift.result
+def summarize(data: T) -> U: ...
+
+
+@lift.result
+def save(data: T, file: Path) -> None: ...
